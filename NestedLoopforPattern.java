d1ebb8456675319b109a4c623e65cb369a31f5a2
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:49:17 +0200

    Add files via upload

---
+++ NestedLoopforPattern.java
@@ -0,0 +1,13 @@
+public class NestedLoopforPattern {             
+public static void main(String[] args) {
+int i ;
+for (i = 1 ; i <= 10 ; i++) {
+for (int j = 1 ; j <= i ; j++) {
+
+    System.out.print("*");
+}
+    System.err.println();
+
+}
+    
+}}
