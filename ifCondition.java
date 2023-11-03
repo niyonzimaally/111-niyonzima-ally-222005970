2292a15f64b57732421ca459deea249ae32de032
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:48:07 +0200

    Add files via upload

---
+++ ifCondition.java
@@ -0,0 +1,30 @@
+public class ifCondition {
+public static void main(String[] args) {
+ int day = 1 ;
+ while (day <= 7) 
+   {
+if (day == 1) {
+System.out.println("the day is monday");
+}
+else if (day == 2) {
+System.out.println("the day is tuesday");
+}
+else if (day == 3) {
+System.out.println("the day is wednesday");
+}
+else if (day == 4) {
+System.out.println("the day is thursday");
+}
+else if (day == 5) {
+    System.out.println("the day is friday");
+}
+else if (day == 6) {
+    System.out.println("the day is saturday");
+}
+else if  (day == 7) {
+    System.out.println("the day is sunday");
+}
+ day++ ;
+}
+}}
+
