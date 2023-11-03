c2615bac0dbfe56f7310eb8ee83651d7e7eae191
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:50:43 +0200

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+public static void main(String[] args) {
+int i ;
+int sum = 0 ;
+i = 1 ;
+while (i <= 20 ) {
+sum += i ;
+System.out.println("the sum of first 20 numbers" + sum + "is: " +sum);
+i++;
+}
+}}
