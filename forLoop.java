d173cb7dcc47b4c37f6f80f1540297fa73f87f41
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:47:41 +0200

    Add files via upload

---
+++ forLoop.java
@@ -0,0 +1,9 @@
+public class forLoop {
+    public static void main(String[] args) {
+        long factorial = 1 ;
+    for (int i = 1 ; i <= 20 ; i++) {
+        factorial *= i ;
+    }
+    System.out.println("the factorial of number is: "  +factorial);
+    }
+}
