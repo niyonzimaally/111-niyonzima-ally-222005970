3da9bc67cedc758d6b9c68355747e53045958ac1
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:47:04 +0200

    Add files via upload
    create program using do...while loop

---
+++ Dowhile.java
@@ -0,0 +1,15 @@
+public class Dowhile  { 
+public static void main(String[] args) {
+    int i ;
+    i = 10 ;
+do { 
+    if (i % 2 == 0) {
+    System.out.println("even numbers between 10 and 30" + i + "is: " +i);
+    }
+    i++ ;
+    }
+while (i <= 30) ;
+
+}
+} 
+
