dbfe5707bc818d85a819703313418a5dd28b5584
niyonzimaally <149528018+niyonzimaally@users.noreply.github.com>
2023-11-03 19:50:00 +0200

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,45 @@
+public class switchCase {
+public static void main(String[] args) {
+ int day = 1; 
+    
+while (day <= 10) {
+switch (day) {
+case 1:
+ System.out.println("Day 1");
+break;
+case 2:
+System.out.println("Day 2");
+  break;
+ case 3:
+System.out.println("Day 3");
+break;
+case 4:
+  System.out.println("Day 4");
+ break;
+                
+ case 5:
+ System.out.println("Day 5");
+break;
+case 6:
+  System.out.println("Day 6");
+ break;
+ case 7:
+System.out.println("Day 7");
+break;
+case 8:
+ System.out.println("Day 8");
+break;
+case 9:
+System.out.println("Day 9");
+break;
+case 10:
+System.out.println("Day 10");
+break;
+default:
+System.out.println("Invalid day");
+break;
+     }
+ day++;
+            }
+        }
+    }
